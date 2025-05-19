1a08c328c3675b8a03bb498b2d27038d7d00d200
Kuroshire <hugo.bounichou@gmail.com>
2025-05-19 21:25:49 +0200

    first commit: Created a React Native app with TypeScript. Created <PokemonCard /> & <PokemonList /> components. Added a base Pokemon Type to the project (will be modified once API fetching is implemented). Created a Home Page.

---
+++ src/components/pokemonCard.tsx
@@ -0,0 +1,31 @@
+import { View, Text, StyleSheet } from "react-native"
+
+type PokemonCardProps = {
+  name: string
+}
+
+export const PokemonCard = ({ name } : PokemonCardProps) => {
+  return (
+    <View style={styles.card}>
+      <Text style={styles.text}>{ name }</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: '#f2f2f2',
+    padding: 16,
+    borderRadius: 12,
+    shadowColor: '#000',
+    shadowOpacity: 0.1,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 5,
+    elevation: 3,
+    marginBottom: 10,
+  },
+  text: {
+    fontSize: 18,
+    color: '#333',
+  },
+});
